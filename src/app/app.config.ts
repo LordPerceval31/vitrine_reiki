4e51acd18fec458eeef8a3a14fe6792bd00b589f
Sylvain <sylvain.de.luca@gmail.com>
2025-06-19 15:38:32 +0200

    SEO finished_temporaly

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,12 +2,13 @@ import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZoneChang
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(), // Gestion des erreurs globales
     provideZoneChangeDetection({ eventCoalescing: true }), // Système de détection des changements
-    provideRouter(routes), // Système de navigation (routes)
+    provideRouter(routes), provideClientHydration(withEventReplay()), // Système de navigation (routes)
     // provideClientHydration() 
   ]
 };
