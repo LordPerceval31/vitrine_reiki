a30c91f7f7af445c03d31e081f58bc83d146dcab
Sylvain <sylvain.de.luca@gmail.com>
2025-06-25 20:35:42 +0200

    Remove environment.local.ts from tracking and fix gitignore path

--- src/app/environments/environment.local.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: false,
-  disableEmailInDev: true,
-  emailjs: {
-    serviceId: 'service_1y56sjj',
-    templateId: 'template_ek5zcbm', 
-    publicKey: 'rwC6rlN3s1wbLsdGb'
-  }
-};
